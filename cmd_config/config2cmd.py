ea1e5734c1aa5e42c3d9631c03330a39b79157a4
ShihHsuanChen <j0018@muenai.com>
2021-10-21 09:36:57 +0800

    add envfile

--- cmd_config/config2cmd.py
+++ cmd_config/config2cmd.py
@@ -1,17 +1,21 @@
+import os
 import subprocess
+from dotenv import load_dotenv
 
 from cmd_config.parser import parse
 
 
-def config2cmd(config_file, run=None):
+def config2cmd(config_file, run=None, envfile=None):
     cmds = parse(config_file)
+    if envfile is not None:
+        load_dotenv(envfile)
 
     if run is None:
         for k, cmd in cmds.items():
             print(f'{k}:',' '.join(cmd))
     elif run in cmds:
-        cmd = cmds[run]
-        subprocess.run(cmd, check=True)
+        cmd = ' '.join(cmds[run])
+        subprocess.run(cmd, check=True, shell=True, env=os.environ)
     else:
         print(f'Cannot find command "{run}"')
         print('Expect either', ', '.join(cmds.keys()))
--- cmd_config/main.py
+++ cmd_config/main.py
@@ -15,9 +15,20 @@ def main():
         default=None,
         help='run command from config file'
     )
+    parser.add_argument(
+        '-e', '--env-file',
+        type=str,
+        default=None,
+        help='environment file (.env)'
+    )
     args = parser.parse_args()
     config_file = args.target
-    config2cmd(args.target, run=args.run)
+
+    config2cmd(
+        args.target,
+        run=args.run,
+        env_file=arg.env_file,
+    )
 
 
 if __name__ == '__main__':
